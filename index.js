5ece2f350e119502d33f26a954ce995a614527d4
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-17 23:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ class Breakfast {
     this.drink = drink;
   }
 }
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.drink = drink;
+    this.soup = soup;
+  }
+}
