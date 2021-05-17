46a5c6aa819f7658e6d620980e10b702a2509d88
Mousa  Rajab <aljawaherimusa@gmail.com>
2021-05-17 23:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Lunch {
 class Dinner {
   constructor(salad, soup, entree, dessert) {
     this.salad = salad;
-    this.drink = drink;
+   
     this.entree = entree;
     this.dessert = dessert;
   }
